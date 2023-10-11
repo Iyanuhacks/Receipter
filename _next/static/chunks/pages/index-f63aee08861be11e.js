81d69b37472137868f7f96c3f4621dab34989560
iyanuhacks <iyanuhacks@gmail.com>
2023-10-11 22:03:00 +0100

    first commit

---
+++ _next/static/chunks/pages/index-f63aee08861be11e.js
@@ -0,0 +1,51 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || [])
+    .push([
+    [405], {
+        8312: function(e, n, t) {
+            (window.__NEXT_P = window.__NEXT_P || [])
+                .push(["/", function() {
+                return t(5075)
+            }])
+        },
+        5075: function(e, n, t) {
+            "use strict";
+            t.r(n), t.d(n, {
+                default: function() {
+                    return a
+                }
+            });
+            var s = t(5893);
+
+            function a() {
+                return (0, s.jsx)(s.Fragment, {
+                    children: (0, s.jsxs)("main", {
+                        className: "px-3 py-5 space-y-5 max-w-sm mx-auto",
+                        children: [(0, s.jsx)("div", {
+                            className: "p-4",
+                            children: (0, s.jsx)("img", {
+                                className: "w-1/5 block mx-auto",
+                                src: "/static/logo.png"
+                            })
+                        }), (0, s.jsx)("h1", {
+                            className: "p-2 text-xs font-bold text-center",
+                            children: "Choose Bank"
+                        }), 
+                        //(0, s.jsx)("h1", {
+//                            className: "p-2 text-xs text-red-600 font-bold text-center",
+ //                           children: "App disabled due to irregular use."
+  //                      }), 
+                        (0, s.jsx)("div", {
+                            id: "pt-10 container-1df8e1025efb5a7d713395d0b17aafa6"
+                        })]
+                    })
+                })
+            }
+        }
+    },
+    function(e) {
+        e.O(0, [774, 888, 179], (function() {
+            return 8312, e(e.s = 8312)
+        }));
+        var n = e.O();
+        _N_E = n
+    }]);
---
+++ _next/static/gC1X-CjB30T96P972hwv1/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = function(s, a, c) {
+    return {
+        __rewrites: {
+            beforeFiles: [],
+            afterFiles: [],
+            fallback: []
+        },
+        "/": ["static/chunks/pages/index-f63aee08861be11e.js"],
+        "/_error": ["static/chunks/pages/_error-a4ba2246ff8fb532.js"],
+        "/access": [s, "static/chunks/pages/access-5c11811fdab4344a.js"],
+        "/firstbank": [s, a, c, "static/chunks/pages/firstbank-c2a3963d0ea2fae5.js"],
+        "/index copy": [a, "static/chunks/545f34e4-a3ff4cfa45838063.js", "static/chunks/pages/index copy-63752072235006be.js"],
+        "/kuda": [s, c, "static/chunks/pages/kuda-234e9f32ef426c36.js"],
+        "/opay": [s, a, "static/chunks/ae51ba48-327c8acbfd7f244d.js", "static/chunks/1a48c3c1-58184a62f8c46a11.js", "static/chunks/pages/opay-559f1a30f7da9bfd.js"],
+        "/uba": [s, c, "static/chunks/pages/uba-29eea7837c1f6086.js"],
+        sortedPages: ["/", "/_app", "/_error", "/access", "/firstbank", "/index copy", "/kuda", "/opay", "/uba"]
+    }
+}("static/chunks/75fc9c18-e61c2e0d9c9a0957.js", "static/chunks/0c428ae2-a1c349499d163f25.js", "static/chunks/675-9ff31190cde414c5.js"), self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
